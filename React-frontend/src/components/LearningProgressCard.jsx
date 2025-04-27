1c51c6179192025f1e0fdae7fa3cba30a99d2896
pramudithayatawara <164331525+pramudithayatawara@users.noreply.github.com>
2025-04-27 11:02:19 +0530

    finalys the learning progress card

--- React-frontend/src/components/LearningProgressCard.jsx
+++ React-frontend/src/components/LearningProgressCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import {
-  Paper, Typography, Box, Chip, Button, IconButton,
+  Paper, Typography, Box, Chip, Button, IconButton,Home,
   LinearProgress, Divider
 } from '@mui/material';
 import {
